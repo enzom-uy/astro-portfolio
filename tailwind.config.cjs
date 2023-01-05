941a51982bc568619fd3d00a4649e62fa639ef6e
enzom-uy <enzom.dev@gmail.com>
2023-01-04 22:40:01 -0300

    Install eslint and setup tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        foreground: "#ebdbb2",
+        background: "#272727",
+        red: "#cc231c",
+        green: "#989719",
+        yellow: "#fabc2e",
+        blue: "#448488",
+        magenta: "#b16185",
+        cyan: "#689d69",
+        white: "#ebdbb2",
+      },
+      fontFamily: {
+        sans: ["JetBrains Mono", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
