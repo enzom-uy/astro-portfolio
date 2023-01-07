c7268c7e7a7f4432e9fa96b7075413b0a9e04980
enzom-uy <enzom.dev@gmail.com>
2023-01-07 01:13:02 -0300

    ...

--- src/stores/locale.ts
+++
@@ -1,8 +0,0 @@
-import { persistentAtom } from "@nanostores/persistent";
-
-export type Locale = "español" | "english";
-
-export const locale = persistentAtom<Locale>("locale", "español", {
-  encode: JSON.stringify,
-  decode: JSON.parse,
-});
