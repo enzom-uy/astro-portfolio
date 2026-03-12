4da499e0925ed95f21f145b725068974dadcd803
enzom-uy <enzom.dev@gmail.com>
2026-03-12 02:34:33 -0300

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [sitemap()],
   site: "https://ojoanalogo.github.io/terminus-astro-template",
   compressHTML: true,
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
